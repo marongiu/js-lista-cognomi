b4559533133da1511935797c88e1b2cfbe6b1b7c
Andrea <andreamarongiu05@gmail.com>
2021-03-04 15:12:58 +0100

    Inserimento array e push

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-console.log("ok");
+// Chiedere all’utente il cognome
+var cognome = prompt("Dammi il tuo cognome:");
+
+// creo un array di cognomi
+
+var cognomi = ["Bianchi", "Rossi", "Perlari", "Lodigiani", "Mastrobattista", "Verdi"]
+
+// inserire il cognome nell'array
+
+cognomi.push(cognome);
+
+// ordino alfabeticamente la lista
